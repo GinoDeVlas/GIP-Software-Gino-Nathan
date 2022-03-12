9f8a1715f2c2d29e9709aabb88af3cff677c2386
Lameme-me <72662310+Lameme-me@users.noreply.github.com>
2022-03-12 15:47:37 +0100

    Log out button
    Gaat de log out button bij dashboard.php terug sturen naar index.php

--- dashboard.php
+++ dashboard.php
@@ -84,7 +84,7 @@ $user_data = check_login($conn);
           </a>
         </li>
         <li class="log_out">
-        <a href="./logout.php">
+        <a href="./index.php">
             <i class='bx bx-log-out'></i>
             <span class="links_name">Log out</span>
           </a>
